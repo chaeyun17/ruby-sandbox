5ed8bbd1a4309a2f169a2c31636bc417ee38201f
Chaeyun Lee <hopefulyun@gmail.com>
2021-04-27 22:28:39 +0900

    클래스 실습

---
+++ basic/LearnClass.rb
@@ -0,0 +1,20 @@
+class Customer
+  @@no_of_customers = 0
+  def initialize(id, name, addr)
+    @cust_id = id
+    @cust_name = name
+    @cust_addr = addr
+    @@no_of_customers += 1
+  end
+
+  def getNo
+    @@no_of_customers
+  end
+
+end
+
+cust1 = Customer.new("1", "john", "wisdom aparts")
+puts cust1.getNo
+cust2 = Customer.new("2", "poul", "new empire")
+puts cust2.getNo
+
---
+++ snippets/symbol-ex.ruby
@@ -0,0 +1,8 @@
+# puts Symbol.all_symbols
+
+# puts "benji".object_id
+# puts "benji".object_id
+# puts :benji.object_id
+# puts :benji.object_id
+
+puts :benji
